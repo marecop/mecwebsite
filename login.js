329aa6532cc652ffc8e3d3e793eb77d67bdba7f1
MEC <yellowenterprice@outlook.com>
2024-12-14 10:13:13 +0800

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
   const password = document.getElementById('password').value;
 
   try {
-    const response = await fetch('/api/users/login', {
+    const response = await fetch('https://mportalm-07a8f9406d21.herokuapp.com/api/users/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
